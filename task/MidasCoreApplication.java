c2623fc03e197734a9468d13922c81c1db7fe0b3
Tajvir Bains <tajvirbains@live.com>
2025-09-06 19:07:11 -0400

    Update MidasCoreApplication.java

--- task/MidasCoreApplication.java
+++ task/MidasCoreApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MidasCoreApplication {
+public class MidasApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MidasCoreApplication.class, args);
+        SpringApplication.run(MidasApplication.class, args);
     }
-
 }
